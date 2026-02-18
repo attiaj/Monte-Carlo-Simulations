75eeec3853954b3f0ee20fa2aa36e82fa02550ce
zbwiebe <zwiebe2024@gmail.com>
2026-02-18 17:11:22 -0500

    zachfinal

--- Homework 1/q1_2.py
+++ Homework 1/q1_2.py
@@ -1,23 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = np.arange(1,500,1)
+# List of progressively larger sample sizes
+N = []
+i = 1
+while i <= 5:
+    N.append(10**i)
+    i += 1
 
-xn = []
-for nx in N:
-    ex = np.random.uniform(0,1)
-    x = -np.log(ex)
+xn = [] # List of approximations of total integral per sample size
+for n in N: # Iterate over sample size
+    ex = np.random.uniform(0,1,n) # Generate n many uniform variables
+    X = -np.log(ex) # Now those variables are between 0 and infinity instead of 0 and 1
     
-    yn = []
-    for ny in N:
-        y = np.random.uniform(0,x,ny)
-        ey = np.exp(-y)
-        inty = sum(ey)/ny
-        yn.append(inty)
+    yn = [] # List of approximations of inner integral per x value
+    for x in X: # Iterate over generated x values
+        y = np.random.uniform(0,x,n) # Generate uniform y in bounds
+        ey = np.exp(-y) # Trnasform uniform to integrand
+        inty = x*sum(ey)/n # Average integrand value times size of integral domain
+        yn.append(inty) # Add to list of inner integrals
     
-    intx = sum(yn)/nx
-    xn.append(intx)
+    intx = sum(yn)/n # Average result of inner integral—sampling by exponential distributions cancels out exterior integrand
+    xn.append(intx) # Add to list of total integrals
 
-plt.plot(N,xn)
-plt.gca().set_ylim([0,2])
-plt.show()
+print(xn[-1]) # Print most accurate value
+plt.plot(N,xn) # Plot approximation vs sample size
+plt.xscale('log') # Rescale x axis
+plt.show() # Show graph
--- Homework 1/q5.py
+++ Homework 1/q5.py
@@ -1,28 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Given constants
 N = 1000
 l = 1/800
 threshold = 50000
 
-sims = 10000
-exceeded = 0
+sims = 10**4 # Number of simulations
+exceeded = 0 # Number of simulations which have so far exceeded the threshold
 
-s = 0
-while s <= sims:
-    claimSum = 0
-    i=0
-    while i < N:
+s = 0 # Simulation index
+while s < sims: # Iterate over all simulations
+    claimSum = 0 # Total claim size in the next month
+    i=0 # ith policy holded
+    while i < N: # Iterate over all policy holders
         p = np.random.uniform(0,1)
-        if p < 0.05:
-            u = np.random.uniform(0,1)
-            ci = -np.log(u)/l
-            claimSum += ci
+        if p < 0.05: # 5% of the time, the ith policy holder will submit a claim
+            u = np.random.uniform(0,1) # Generate uniform
+            ci = -np.log(u)/l # Transform uniform into exponentially distributed claim size
+            claimSum += ci # Add ith policy holder's claim to the total
         i += 1
-    if claimSum >= threshold:
+    if claimSum >= threshold: # Count the number of times the sum of claims is above the threshold
         exceeded += 1
     s += 1
 
-var = exceeded/sims
+var = exceeded/sims # Percentage of times the sum of claims is above the threshold
 
 print(var)
